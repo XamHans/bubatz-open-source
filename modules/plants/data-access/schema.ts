36809b1806c2e21c7581d79abfc9bb365c935fb6
Gonçalo Almeida <goncalo.almeida@techtideport.org>
2024-05-29 14:14:48 +0100

    prettier

--- modules/plants/data-access/schema.ts
+++ modules/plants/data-access/schema.ts
@@ -1,34 +1,34 @@
-import { transactions } from "@/modules/sales/data-access/schema";
-import { pgTable, real, serial, text } from "drizzle-orm/pg-core";
-import { relations } from "drizzle-orm/relations";
-import { createInsertSchema, createSelectSchema } from "drizzle-zod";
-import { z } from "zod";
+import { transactions } from '@/modules/sales/data-access/schema';
+import { pgTable, real, serial, text } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm/relations';
+import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
+import { z } from 'zod';
 
 export const plants = pgTable('transactions', {
-    id: serial('id').primaryKey(),
-    name: text('quantity').notNull(),
-    price: real('price').notNull(),
+  id: serial('id').primaryKey(),
+  name: text('quantity').notNull(),
+  price: real('price').notNull(),
 });
 
-export const plantsRelations = relations(plants, ({many}) => ({
-    transactions: many(transactions),
-}))
+export const plantsRelations = relations(plants, ({ many }) => ({
+  transactions: many(transactions),
+}));
 
 /**
  * * Input schemas
  */
 
 export const createPlantInputSchema = createInsertSchema(plants, {
-    /**
-     * TODO: To be implemented
-     */
+  /**
+   * TODO: To be implemented
+   */
 });
 
 export const getPlantInputSchema = createSelectSchema(plants, {
-    /**
-     * TODO: To be implemented
-     */
+  /**
+   * TODO: To be implemented
+   */
 });
 
-export type CreatePlantInput = z.infer<typeof createPlantInputSchema>
-export type GetPlantInput = z.infer<typeof getPlantInputSchema>
+export type CreatePlantInput = z.infer<typeof createPlantInputSchema>;
+export type GetPlantInput = z.infer<typeof getPlantInputSchema>;
