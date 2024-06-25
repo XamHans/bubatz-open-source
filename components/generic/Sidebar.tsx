3311eaf05d373aa2c5cd835e2b430f74607afbdf
Johannes Hayer <johannes.mueller@avl.com>
2024-06-25 11:49:58 +0200

    refactor: Update Sidebar component to use lazy loading for images

--- components/generic/Sidebar.tsx
+++ components/generic/Sidebar.tsx
@@ -1,7 +1,9 @@
-import Link from 'next/link';
+'use client';
 
 import { cn } from '@/lib/utils';
 import { Leaf, Package2, Settings, Users2 } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import {
   Tooltip,
   TooltipContent,
@@ -14,6 +16,9 @@ type Props = {
 };
 
 const Sidebar = ({ className }: Props) => {
+  const pathname = usePathname();
+  const isActive = (path: string) => pathname === path;
+
   return (
     <div className={cn(className)}>
       <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
@@ -23,7 +28,10 @@ const Sidebar = ({ className }: Props) => {
               <TooltipTrigger asChild>
                 <Link
                   href="/sales"
-                  className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full text-lg font-semibold text-muted-foreground hover:text-foreground md:h-8 md:w-8 md:text-base"
+                  className={cn(
+                    'group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full text-lg font-semibold text-muted-foreground hover:text-foreground md:h-8 md:w-8 md:text-base',
+                    isActive('/sales') && 'bg-black',
+                  )}
                 >
                   <Package2 className="h-4 w-4 transition-all" />
                   <span className="sr-only">Sales</span>
@@ -35,7 +43,10 @@ const Sidebar = ({ className }: Props) => {
               <TooltipTrigger asChild>
                 <Link
                   href="/plants"
-                  className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                  className={cn(
+                    'flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8',
+                    isActive('/plants') && 'bg-black',
+                  )}
                 >
                   <Leaf className="h-5 w-5 transition-all" />
                   <span className="sr-only">Plants</span>
@@ -47,7 +58,10 @@ const Sidebar = ({ className }: Props) => {
               <TooltipTrigger asChild>
                 <Link
                   href="/members"
-                  className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                  className={cn(
+                    'flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8',
+                    isActive('/members') && 'bg-black',
+                  )}
                 >
                   <Users2 className="h-5 w-5" />
                   <span className="sr-only">Members</span>
@@ -61,7 +75,10 @@ const Sidebar = ({ className }: Props) => {
               <TooltipTrigger asChild>
                 <Link
                   href="#"
-                  className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                  className={cn(
+                    'flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8',
+                    isActive('#') && 'bg-black',
+                  )}
                 >
                   <Settings className="h-5 w-5" />
                   <span className="sr-only">Settings</span>
